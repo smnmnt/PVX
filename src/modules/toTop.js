493bc8f03187bf343d6bbb677064d3f557968611
smnmnt <o.kosobreev@yandex.ru>
2022-04-29 23:43:21 +0300

    add disapearing totop

--- src/modules/toTop.js
+++ src/modules/toTop.js
@@ -1,10 +1,24 @@
 const toTop = () => {
     const smoothScrollBtn = document.querySelector('.smooth-scroll')
 
-    smoothScrollBtn.addEventListener('click', topFunction)
+    smoothScrollBtn.style.display = 'none'
+    
+    const trackingScroll = () => {
+        let scrolled = window.scrollY
+        let coordinates = document.documentElement.clientHeight
+        
+        if (scrolled > coordinates) {
+            smoothScrollBtn.style.display = 'block'
+        }else if (scrolled < coordinates) {
+            smoothScrollBtn.style.display = 'none'
+        }
+    }
 
     function topFunction() {
         window.scrollTo({top: 0, behavior: 'smooth'})
     }
+
+    smoothScrollBtn.addEventListener('click', topFunction)
+    window.addEventListener('scroll', trackingScroll)
 }
 export default toTop
