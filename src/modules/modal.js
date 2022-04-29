3b13e5d3556c9d04cefecb4ed29e36fc3f3ff145
smnmnt <o.kosobreev@yandex.ru>
2022-04-29 17:45:36 +0300

    service modal added

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,26 +1,36 @@
 const modal = () => {
-    const modal = document.querySelector('.header-modal')
+    const headerModal = document.querySelector('.header-modal')
+    const serviceModal = document.querySelector('.services-modal')
+
     const overlay = document.querySelector('.overlay')
-    const closeModalBtn = document.querySelector('.header-modal__close')
+
+    const closeHeaderModalBtn = document.querySelector('.header-modal__close')
+    const closeServiceModalBtn = document.querySelector('.services-modal__close')
     const fancyBtns = document.querySelectorAll('.fancyboxModal')
 
     fancyBtns.forEach(btn => {
         btn.addEventListener('click', (e) => {
             if (btn.hash === '#callback') {
-                console.log('#callback');
-                modal.style.display = 'block'
+                // console.log('#callback');
+                headerModal.style.display = 'block'
                 overlay.style.display = 'block'
             } else if (btn.hash === '#application') {
-                console.log('no application');
+                // console.log('no application');
+                serviceModal.style.display = 'block'
+                overlay.style.display = 'block'
+                
             }
         })
-        
     })
 
-    closeModalBtn.addEventListener('click', () => {
-        modal.style.display = 'none'
+    closeHeaderModalBtn.addEventListener('click', () => {
+        headerModal.style.display = 'none'
         overlay.style.display = 'none'
-        console.log('btnaction off');
     })
+    closeServiceModalBtn.addEventListener('click', () => {
+        serviceModal.style.display = 'none'
+        overlay.style.display = 'none'
+    })
+    
 }
 export default modal
