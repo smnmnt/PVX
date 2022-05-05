ad35aebb08e7e7dcaabfd45ae3b1966c1788d304
smnmnt <o.kosobreev@yandex.ru>
2022-05-05 17:14:19 +0300

    modalwidow for docs

--- src/modules/imageZoom.js
+++ src/modules/imageZoom.js
@@ -1,45 +1,55 @@
 const imageZoom = () => {
-    const style = '.modal-img { z-index: 99; position: absolute; top: 2%; left: 33%; background: url(../images/documents/original/document4.jpg) center / cover; width: 654px; height: 900px;}'
+    const style = '.modal-img { z-index: 99; position: fixed; top: 2%; left: 33%; background: url(../images/documents/original/document4.jpg) center / cover; width: 654px; height: 900px; transition: 200ms;}'
     const styleCreate = document.createElement('style')
     styleCreate.innerText = style
     document.head.appendChild(styleCreate)
 
     const overlay = document.querySelector('.overlay')
-
+    const body = document.querySelector('body')
+    
     const imgs = document.querySelectorAll('.sertificate-document');
     const disableImgAttributes = document.querySelectorAll('.sertificate-document')
-    const modalImg = document.querySelector(".sertificate-document > .img-responsive")
-    const modalImgAttr = modalImg.getAttribute('src')
-
-    const hoverEffectImgs = document.querySelectorAll('.document-overlay')
-
+    
+    const hoverEffectImgs = document.querySelectorAll('.img-responsive')
+    
     hoverEffectImgs.forEach(hoverEffectImg => {
         hoverEffectImg.addEventListener('mouseover', () => {
-            hoverEffectImg.style.transition = '.2s'
-            hoverEffectImg.style.opacity = "1"
+            hoverEffectImg.style.transition = '0.2'
+            hoverEffectImg.style.opacity = "0.5"
         })
         hoverEffectImg.addEventListener('mouseout', () => {
-            hoverEffectImg.style.transition = '.2s'
+            hoverEffectImg.style.transition = '0.2'
             hoverEffectImg.style.opacity = "0"
         })
-    }) 
-
+    })
+    
     disableImgAttributes.forEach(disableImgAttributes => {
         disableImgAttributes.setAttribute('href', '')
     })
     
+    const addModalDocument = () => {
+        overlay.style.display = "block";
+        const modalDiv = document.createElement('div')
+        modalDiv.classList.add('modal-img')
+        body.append(modalDiv)
+        modalDiv.innerHTML = `
+        <span title="Close" class="header-modal__close">x</span>
+        `
+        document.addEventListener('click', (e) => {
+            if (e.target.closest('.overlay') || e.target.closest(".header-modal__close")) {
+                modalDiv.remove()
+                overlay.style.display = 'none'
+            }
+
+        })
+    }
+    
     imgs.forEach(imgDiv => {
-        
         imgDiv.addEventListener('click', (e) => {
             e.preventDefault()            
-            overlay.style.display = "block";
-            const modalDiv = document.createElement('div')
-            modalDiv.classList.add('modal-img')
-            overlay.append(modalDiv)
-            console.log(modalDiv);
+            addModalDocument()
         })
-    })
-
+    })    
 }
-
+    
 export default imageZoom
